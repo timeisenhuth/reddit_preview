60ad53c191292b9d4da90c4d5ac5225bac77066e
Tim Eisenhuth <tim.eisenhuth@gmail.com>
2013-04-15 15:41:03 +1000

    Avoid on javascript links

--- preview.js
+++ preview.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
   $('.entry a').click(function(event){
     href = $(this).attr('href');
-    //if ( href.match('imgur') ) {
+    if ( !href.match('javascript:') ) {
       $('#redditpreview').remove();
       $(this).after('<div id="redditpreview"><a href="'+href+'">'+href+'</a><div class="notice">Press escape to close</div><iframe src='+href+' width="500" height="500"></iframe></div>');
       $('#redditpreview').css('left', event.pageX+10);
@@ -13,7 +13,7 @@ $(document).ready(function(){
       $('#redditpreview').css('display', 'inline');
       $('#redditpreview').css('position', 'absolute');
       return false;
-    //}
+    }
     return false;
   });
   
